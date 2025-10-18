f2d67058008187aa88a0e3a7879e34c0f5da4460
AlwinD <edudavilaraffo@gmail.com>
2025-10-18 08:03:32 -0500

    add cart route

--- src/modules/home/routes/AppRoutes.tsx
+++ src/modules/home/routes/AppRoutes.tsx
@@ -2,17 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../page/HomePage";
 import { MainLayout } from "../layout/MainLayout";
 import CatalogRoutes from "../../catalog/routes/CatalogRoutes";
+import CartCheckoutRoutes from "../../cart-checkout/routes/CartCheckoutRoutes";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <MainLayout>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          
+         <Route path="/" element={<HomePage />} />
           {/* Rutas del módulo de catálogo */}
           <Route path="/catalog/*" element={<CatalogRoutes />} />        
-          {/* Aquí agregas las rutas de otros módulos */}
+          <Route path="/*" element={<CartCheckoutRoutes />} />
+{/* Aquí agregas las rutas de otros módulos */}
           {/* <Route path="/cart" element={<CartPage />} /> */}
         </Routes>
       </MainLayout>
